cc1378899ec28d5e060d1645afd3b1f9943ce109
Julia Wiater <juliaw@student.agh.edu.pl>
2023-05-24 22:12:53 +0200

    uproszczenie funkcji liczącej

--- liczenie.py
+++ liczenie.py
@@ -1,12 +1,9 @@
 # rozwiazywanie wyrazenia z traktowaniem znaku "^" jako potegi
 def result(expression):
     n = expression.find("^")
+
     while n != -1:
-        lst = list(expression) + ['']
-        lst[n] = lst[n+1] = "*"
-        for i in range(n+2, len(lst)):
-            lst[i] = expression[i-1]
-        expression = ''.join(lst)
+        expression = expression[:n] + "**" + expression[n+1:]
         n = expression.find("^")
     print(expression)
     print(eval(expression))
