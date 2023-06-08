d34b41dae481eaede739ad16c62f85f842bab251
grabarz128 <grabarz128@gmail.com>
2023-06-08 09:43:28 +0200

    dodanie wpisywania z okna

--- kalkulator.py
+++ kalkulator.py
@@ -38,22 +38,22 @@ def calc_cell(window): #komórka do wpisywania obliczeń
 def calc_buttons(window, cell): #ustawia ustawia cyfry i operatory działań
     buttons = []
 
-    button_7 = tk.Button(window, text='7', bg='white', borderwidth=0)
+    button_7 = tk.Button(window, text='7', bg='white', borderwidth=0, command=lambda: put_button(' 7 '))
     button_7.grid(row=3, column=0, ipadx=10, ipady=5)
     button_7.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_7)
 
-    button_8 = tk.Button(window, text='8', bg='white', borderwidth=0)
+    button_8 = tk.Button(window, text='8', bg='white', borderwidth=0, command=lambda: put_button(' 8 '))
     button_8.grid(row=3, column=1, ipadx=10, ipady=5)
     button_8.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_8)
 
-    button_9 = tk.Button(window, text='9', bg='white', borderwidth=0)
+    button_9 = tk.Button(window, text='9', bg='white', borderwidth=0, command=lambda: put_button(' 9 '))
     button_9.grid(row=3, column=2, ipadx=10, ipady=5)
     button_9.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_9)
 
-    button_divide = tk.Button(window, text='/', bg='white', borderwidth=0)
+    button_divide = tk.Button(window, text='/', bg='white', borderwidth=0, command=lambda: put_button(' / '))
     button_divide.grid(row=3, column=3, ipadx=10, ipady=5)
     button_divide.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_divide)
@@ -68,52 +68,52 @@ def calc_buttons(window, cell): #ustawia ustawia cyfry i operatory działań
     button_clr.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_clr)
 
-    button_4 = tk.Button(window, text='4', bg='white', borderwidth=0)
+    button_4 = tk.Button(window, text='4', bg='white', borderwidth=0, command=lambda: put_button(' 4 '))
     button_4.grid(row=4, column=0, ipadx=10, ipady=5)
     button_4.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_4)
 
-    button_5 = tk.Button(window, text='5', bg='white', borderwidth=0)
+    button_5 = tk.Button(window, text='5', bg='white', borderwidth=0, command=lambda: put_button(' 5 '))
     button_5.grid(row=4, column=1, ipadx=10, ipady=5)
     button_5.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_5)
 
-    button_6 = tk.Button(window, text='6', bg='white', borderwidth=0)
+    button_6 = tk.Button(window, text='6', bg='white', borderwidth=0, command=lambda: put_button(' 6 '))
     button_6.grid(row=4, column=2, ipadx=10, ipady=5)
     button_6.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_6)
 
-    button_multiply = tk.Button(window, text='*', bg='white', borderwidth=0)
+    button_multiply = tk.Button(window, text='*', bg='white', borderwidth=0, command=lambda: put_button(' * '))
     button_multiply.grid(row=4, column=3, ipadx=10, ipady=5)
     button_multiply.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_multiply)
 
-    button_leftbracket = tk.Button(window, text='(', bg='white', borderwidth=0)
+    button_leftbracket = tk.Button(window, text='(', bg='white', borderwidth=0, command=lambda: put_button(' ( '))
     button_leftbracket.grid(row=4, column=4, ipadx=10, ipady=5)
     button_leftbracket.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_leftbracket)
 
-    button_rightbracket = tk.Button(window, text=')', bg='white', borderwidth=0)
+    button_rightbracket = tk.Button(window, text=')', bg='white', borderwidth=0,command=lambda: put_button(' ) '))
     button_rightbracket.grid(row=4, column=5, ipadx=10, ipady=5)
     button_rightbracket.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_rightbracket)
 
-    button_1 = tk.Button(window, text='1', bg='white', borderwidth=0)
+    button_1 = tk.Button(window, text='1', bg='white', borderwidth=0, command=lambda: put_button(' 1 '))
     button_1.grid(row=5, column=0, ipadx=10, ipady=5)
     button_1.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_1)
 
-    button_2 = tk.Button(window, text='2', bg='white', borderwidth=0)
+    button_2 = tk.Button(window, text='2', bg='white', borderwidth=0, command=lambda: put_button(' 2 '))
     button_2.grid(row=5, column=1, ipadx=10, ipady=5)
     button_2.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_2)
 
-    button_3 = tk.Button(window, text='3', bg='white', borderwidth=0)
+    button_3 = tk.Button(window, text='3', bg='white', borderwidth=0, command=lambda: put_button(' 3 '))
     button_3.grid(row=5, column=2, ipadx=10, ipady=5)
     button_3.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_3)
 
-    button_minus = tk.Button(window, text='/', bg='white', borderwidth=0)
+    button_minus = tk.Button(window, text='/', bg='white', borderwidth=0, command=lambda: put_button(' / '))
     button_minus.grid(row=5, column=3, ipadx=10, ipady=5)
     button_minus.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_minus)
@@ -128,22 +128,22 @@ def calc_buttons(window, cell): #ustawia ustawia cyfry i operatory działań
     button_sqrt.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_sqrt)
 
-    button_0 = tk.Button(window, text='0', bg='white', borderwidth=0)
+    button_0 = tk.Button(window, text='0', bg='white', borderwidth=0, command=lambda: put_button(' 0 '))
     button_0.grid(row=6, column=0, ipadx=10, ipady=5)
     button_0.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_0)
 
-    button_comma = tk.Button(window, text=',', bg='white', borderwidth=0)
+    button_comma = tk.Button(window, text=',', bg='white', borderwidth=0, command=lambda: put_button(' , '))
     button_comma.grid(row=6, column=1, ipadx=10, ipady=5)
     button_comma.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_comma)
 
-    button_percent = tk.Button(window, text='%', bg='white', borderwidth = 0)
+    button_percent = tk.Button(window, text='%', bg='white', borderwidth = 0, command=lambda: put_button(' % '))
     button_percent.grid(row=6, column=2, ipadx=10, ipady=5)
     button_percent.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_percent)
 
-    button_plus = tk.Button(window, text='+', bg='white', borderwidth=0)
+    button_plus = tk.Button(window, text='+', bg='white', borderwidth=0,command=lambda: put_button(' + '))
     button_plus.grid(row=6, column=3, ipadx=10, ipady=5)
     button_plus.configure(width=5, height=2, font=("Calibri", 13))
     buttons.append(button_plus)
@@ -162,6 +162,12 @@ def calc_buttons(window, cell): #ustawia ustawia cyfry i operatory działań
         margin = 0 if len(symbols[i]) == 1 else 10
         buttons[i].grid(row=j, column=i % 6,  ipady=5)
 '''
+
+def put_button(sign):    #funkcja dodająca wcisniete przyciski
+    expression = cell.get()
+    res = str(expression) + sign
+    cell.delete(0, tk.END)
+    cell.insert(tk.END, str(res))
 
 def calculate(cell):    #funkcja obliczająca wyrażenie
     expression = cell.get()
